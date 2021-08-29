7d9b8953403d176dbcfb131b6c3f97d69658c0dc
Janglz <wpidish@gmail.com>
2021-08-29 20:35:54 +0300

    CORS(((

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,7 +21,7 @@ class Main extends React.Component {
     }
 
     getData = async (rows) => {
-        const data = await fetch(`http://www.filltext.com/?rows=${rows}&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`).
+        const data = await fetch(`https://www.filltext.com/?rows=${rows}&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`).
             then(response => response.json())
         return data;
     }
@@ -36,19 +36,19 @@ class Main extends React.Component {
         const users = await this.findUsersOnCurrentPage([...this.state.users]);
         await this.setState({ usersToShow: users, })
     }
-    
+
     selectRows = async (e) => {
         this.setState({
             rows: e.target.value,
             isLoaded: false,
         });
         try {
-            await this.getData(e.target.value).then(result=>{
+            await this.getData(e.target.value).then(result => {
                 this.setState({
                     data: result,
                     users: result,
                     isLoaded: true,
-                })  
+                })
             });
             await this.selectPage(1)
         } catch (e) {
@@ -64,18 +64,18 @@ class Main extends React.Component {
             users: filtered,
         })
         await this.selectPage(1)
-        
+
     }
 
     findUsersOnCurrentPage = (users) => {
         const offset = this.state.page * 50;
-        return [...users.slice(offset - 50, offset)];    
+        return [...users.slice(offset - 50, offset)];
     }
 
     search = async (users, query) => {
         if (!query || query === '') return users;
 
-        let filteredUsers = await users.filter((user) => Object.values(user).find(entry=> {
+        let filteredUsers = await users.filter((user) => Object.values(user).find(entry => {
             return String(entry).indexOf(query) !== -1;
         }));
         return filteredUsers;
@@ -85,30 +85,30 @@ class Main extends React.Component {
         const value = e.currentTarget.getAttribute('value');
         const sortedBy =
             this.state.sortedBy === value ?
-            `${value}-reverse` : value
+                `${value}-reverse` : value
         const bool =
             sortedBy === value ?
-            1 : -1;
+                1 : -1;
 
         const sortByValue = (first, second) => first[value] > second[value] ? bool : -(bool)
         const sortedAllUsers = await [...this.state.data].sort(sortByValue);
 
-        await this.setState( {
-            sortedBy: sortedBy, 
+        await this.setState({
+            sortedBy: sortedBy,
         })
-        if (this.state.searchQuery === '' || this.state.searchQuery === null) {  
-            await this.setState({ 
-                data: await sortedAllUsers, 
+        if (this.state.searchQuery === '' || this.state.searchQuery === null) {
+            await this.setState({
+                data: await sortedAllUsers,
                 users: sortedAllUsers,
                 usersToShow: await this.findUsersOnCurrentPage(sortedAllUsers)
             })
         } else {
-            const filtered = await this.search(sortedAllUsers, this.state.searchQuery); 
-            const currentPageUsers = await this.findUsersOnCurrentPage(filtered);           
-            await this.setState({ 
-                usersToShow: await currentPageUsers,     
+            const filtered = await this.search(sortedAllUsers, this.state.searchQuery);
+            const currentPageUsers = await this.findUsersOnCurrentPage(filtered);
+            await this.setState({
+                usersToShow: await currentPageUsers,
                 users: filtered,
-            })  
+            })
         }
     }
 
@@ -118,8 +118,8 @@ class Main extends React.Component {
         await this.setState({
             searchQuery: query,
         })
-        const currentPageUsers = await this.findUsersOnCurrentPage([...this.state.data]);  
-        const filtered = await this.search(currentPageUsers, this.state.searchQuery);  
+        const currentPageUsers = await this.findUsersOnCurrentPage([...this.state.data]);
+        const filtered = await this.search(currentPageUsers, this.state.searchQuery);
         await this.setState({
             usersToShow: filtered,
         })
@@ -127,35 +127,35 @@ class Main extends React.Component {
 
     searchUsers = async () => { //поиск по всем юзерам
         const query = this.state.searchQuery;
-        const filtered = await this.search([...this.state.data], query); 
+        const filtered = await this.search([...this.state.data], query);
         await this.setState({
             users: filtered,
         })
-        await this.selectPage(1) 
+        await this.selectPage(1)
     }
 
     render() {
         if (!this.state.isLoaded) return <Spinner />
-        const container = !this.state.rows ? 
-        (<h1>Выбирайте кнопочку...</h1>)
-        :
-        (<div>
-            <Pagination
-                page={this.state.page}
-                pages={this.state.pages}
-                onSelectPage={this.selectPage}
-            />
-            <Table
-                isLoaded={this.state.isLoaded}
-                sortedBy={this.state.sortedBy}
-                users={this.state.usersToShow}
-                searchQuery={this.state.searchQuery}
-                onAddUser={this.addUser}
-                onFilter={this.filterUsers}
-                onSearch={this.searchUsers}
-                onSort={this.sortUsers}
-            />
-        </div>);
+        const container = !this.state.rows ?
+            (<h1>Выбирайте кнопочку...</h1>)
+            :
+            (<div>
+                <Pagination
+                    page={this.state.page}
+                    pages={this.state.pages}
+                    onSelectPage={this.selectPage}
+                />
+                <Table
+                    isLoaded={this.state.isLoaded}
+                    sortedBy={this.state.sortedBy}
+                    users={this.state.usersToShow}
+                    searchQuery={this.state.searchQuery}
+                    onAddUser={this.addUser}
+                    onFilter={this.filterUsers}
+                    onSearch={this.searchUsers}
+                    onSort={this.sortUsers}
+                />
+            </div>);
         return (
             <div>
                 <button onClick={this.selectRows} value="32">Поменьше...</button>
